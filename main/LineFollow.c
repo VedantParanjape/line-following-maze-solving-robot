628e38b06d4638f4540cefd7701434f1f14386d9
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2019-08-13 21:57:29 +0530

    working version

--- main/LineFollow.c
+++ main/LineFollow.c
@@ -7,9 +7,8 @@
 #include "TUNING.h"
 
 adc1_channel_t channel[4] = {ADC_CHANNEL_7, ADC_CHANNEL_6, ADC_CHANNEL_0, ADC_CHANNEL_3};
-// adc1_channel_t turn_channel[2] = {ADC_CHANNEL_4, ADC_CHANNEL_5};
 int weights[4] = {3,1,-1,-3};
-
+int q,p,r;
 /*
  * Line Following PID Constants
  */
@@ -20,9 +19,9 @@ float kD = 2.0;
 /*
  * Motor value constraints
  */
-float opt = 70;
+float opt = 80;
 float lower_pwm_constrain = 60;
-float higher_pwm_constrain = 100;
+float higher_pwm_constrain = 80;
 float left_pwm = 0, right_pwm = 0;
 
 /*
@@ -32,8 +31,8 @@ float error=0, prev_error, difference, cumulative_error, correction;
 
 uint32_t adc_reading[4];
 float sensor_value[4];
-// uint32_t adc_turn_reading[2];
-// float sensor_turn_value[2];
+uint32_t adc_turn_reading[2];
+float sensor_turn_value[2];
 
 static void read_sensors()
 {
@@ -44,13 +43,6 @@ static void read_sensors()
     }
 }
 
-// static void read_turn_sensors()
-// {
-//     adc_turn_reading[0] = adc1_get_raw(turn_channel[0]);
-//     adc_turn_reading[1] = adc1_get_raw(turn_channel[1]);
-//     printf("sensor [0]: %d sensor[1]: %d", adc_turn_reading[0], adc_turn_reading[1]);
-// }
-
 static void calc_sensor_values()
 {
     for(int i = 0; i < 4; i++)
@@ -60,17 +52,6 @@ static void calc_sensor_values()
         // printf("fil[%d]: %f ", i, sensor_value[i]); 
     }
 }
-
-// static void calc_turn_sensor_values()
-// {
-//     for(int i = 0; i < 2; i++)
-//     {
-//         sensor_turn_value[i] = map(adc_turn_reading[i], 1700, 4000, 0, 1000);
-//         sensor_turn_value[i] = constrain(sensor_turn_value[i],0,1000);
-//         // printf("fil[%d]: %f ", i, sensor_turn_value[i]); 
-//     }
-//     printf("\n");
-// }
 
 static void calculate_error()
 {
@@ -94,13 +75,13 @@ static void calculate_error()
         pos = weighted_sum / sum;
     }
 
-    if(all_black_flag == 1)
-    {
-        if(error > 0)
-            pos = 2.5;
-        else
-            pos = -2.5;
-    }
+    // if(all_black_flag == 1)
+    // {
+    //     if(error > 0)
+    //         pos = 2.5;
+    //     else
+    //         pos = -2.5;
+    // }
 
     error = pos;
 }
@@ -125,118 +106,141 @@ static void calculate_correction()
     prev_error = error;
 }
 
-void followpath()
-{
-    while (1)
-    {
-
-        calculate_error();
-        calculate_correction();
-        
-        left_pwm = constrain((opt + correction), lower_pwm_constrain, higher_pwm_constrain);
-        right_pwm = constrain((opt - correction), lower_pwm_constrain, higher_pwm_constrain);
-         
-        for(int i=0;i<4;i++)
-        {
-            printf("sensor value[%d]: %f  ", i, sensor_value[i]);
-        }    
-        bot_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, left_pwm, right_pwm);   
-
-
-        if(sensor_value[0] < 100 && sensor_value[0] >=0 && sensor_value[1] > 900 && sensor_value[1] <= 1000 && sensor_value[2] > 900 && sensor_value[2] <= 1000 && sensor_value[3] > 900 && sensor_value[3] <= 1000)
-         {
-            break;
-         }
-        else if (sensor_value[0] > 900 && sensor_value[0] <=1000 && sensor_value[1] > 900 && sensor_value[1] <= 1000 && sensor_value[2] > 900 && sensor_value[2] <= 1000 && sensor_value[3] < 100 && sensor_value[3] >=0) 
-        {
-            break;
-        }
-        else if (sensor_value[0] < 100 && sensor_value[0] >= 0 && sensor_value[1] < 100 && sensor_value[1] >= 0 && sensor_value[2] < 100 && sensor_value[2] >= 0 && sensor_value[3] < 100 && sensor_value[3] >= 0)
-        { 
-            break;
-        }      
-    }
-}
-
-
 void turnbot(char d)
 {
     if (d == 'L')
     {
-        vTaskDelay(100/portTICK_PERIOD_MS); 
-        bot_spot_left(MCPWM_UNIT_0, MCPWM_TIMER_0,90,90);
-        vTaskDelay(140/portTICK_PERIOD_MS); 
+        while (q==1)
+        {   
+            read_sensors();
+            calc_sensor_values();
+
+            if((sensor_value[0] > 700) || (sensor_value[1] > 700) || (sensor_value[2] > 700) || (sensor_value[3] > 700))
+            {
+                q=0;
+            }
+            
+            bot_spot_left(MCPWM_UNIT_0, MCPWM_TIMER_0,85,85);
+        } 
     }
 
     else if(d == 'R')
     {
-        vTaskDelay(100/portTICK_PERIOD_MS);
-        bot_spot_right(MCPWM_UNIT_0, MCPWM_TIMER_0,90,90);
-        vTaskDelay(140/portTICK_PERIOD_MS);
+        while (p==1)
+        {   
+            read_sensors();
+            calc_sensor_values();
+
+            if((sensor_value[0] > 700) || (sensor_value[1] > 700) || (sensor_value[2] > 700) || (sensor_value[3] > 700))
+            {
+                p=0;
+            }
+            
+            bot_spot_right(MCPWM_UNIT_0, MCPWM_TIMER_0,85,85);
+        } 
+    
     }
 
     else if (d == 'B')
-    {
-        vTaskDelay(100/portTICK_PERIOD_MS);
-        bot_spot_left(MCPWM_UNIT_0, MCPWM_TIMER_0,90,90);
-        vTaskDelay(240/portTICK_PERIOD_MS);
-    }
+    { 
+        while (r==1)
+        {   
+            read_sensors();
+            calc_sensor_values();
+
+            if((sensor_value[0] > 700) || (sensor_value[1] > 700) || (sensor_value[2] > 700) || (sensor_value[3] > 700))
+            {
+                r=0;
+            }
+
+            bot_spot_right(MCPWM_UNIT_0, MCPWM_TIMER_0,85,85);
+        } 
+    
+    } 
+}
+
+void odometry()
+{
+
 }
 
 
 void line_follow_task(void *arg)
 {
-  mcpwm_i<nitialize();
+  mcpwm_initialize();
   char turn;
   
   while(1)
   {
     read_sensors();
-    // read_turn_sensors();
     calc_sensor_values();
-    // calc_turn_sensor_values();
+    calculate_error();
+    calculate_correction();
+        
+    left_pwm = constrain((opt + correction), lower_pwm_constrain, higher_pwm_constrain);
+    right_pwm = constrain((opt - correction), lower_pwm_constrain, higher_pwm_constrain);
+         
+    for(int i=0;i<4;i++)
+    {
+        printf("sensor value[%d]: %f  ", i, sensor_value[i]);
+    }    
+    printf("\n");
+    bot_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, left_pwm, right_pwm);  
 
-    followpath();
+    if(sensor_value[0] < 400  && sensor_value[1] > 900  && sensor_value[2] > 900  && sensor_value[3] > 900)
+    {
+        turn = 'L';
+        printf("turn detected: %c  ", turn);
+        q=1;
 
-// add function to stop a bit
-       if(sensor_value[0] < 100 && sensor_value[0] >=0 && sensor_value[1] > 900 && sensor_value[1] <= 1000 && sensor_value[2] > 900 && sensor_value[2] <= 1000 && sensor_value[3] > 900 && sensor_value[3] <= 1000)
-       {
-         turn = 'L';
-         printf("turn detected: %c  ", turn);
-         turnbot(turn);
+        vTaskDelay(200/portTICK_PERIOD_MS);
+        bot_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
+        vTaskDelay(200/portTICK_PERIOD_MS);    
+        turnbot(turn);
 
-         for(int i=0;i<4;i++)
-         {
-             printf("sensor value[%d]: %f  ", i, sensor_value[i]);
-         }
-         printf("\n");
-       }
-
-       else if (sensor_value[0] > 900 && sensor_value[0] <=1000 && sensor_value[1] > 900 && sensor_value[1] <= 1000 && sensor_value[2] > 900 && sensor_value[2] <= 1000 && sensor_value[3] < 100 && sensor_value[3] >=0) 
-       {
-         turn = 'R';
-         printf("turn detected: %c  ", turn);
-         turnbot(turn);
-
-         for(int i=0;i<4;i++)
-         {
-             printf("sensor value[%d]: %f  ", i, sensor_value[i]);
-         }
-         printf("\n");
-       }
-       else if (sensor_value[0] < 100 && sensor_value[0] >= 0 && sensor_value[1] < 100 && sensor_value[1] >= 0 && sensor_value[2] < 100 && sensor_value[2] >= 0 && sensor_value[3] < 100 && sensor_value[3] >= 0)
-       {
-         turn = 'B';
-         printf("turn detected: %c  ", turn);
-         turnbot(turn);
-
-         for(int i=0;i<4;i++)
-         {
-             printf("sensor value[%d]: %f  ", i, sensor_value[i]);
-         }
-         printf("\n");
-       }
+        for(int i=0;i<4;i++)
+        {
+            printf("sensor value[%d]: %f  ", i, sensor_value[i]);
+        }
+        printf("\n");
     }
+
+    else if (sensor_value[0] > 900 && sensor_value[1] > 900 && sensor_value[2] > 900  && sensor_value[3] < 400) 
+    {
+        turn = 'R';
+        printf("turn detected: %c  ", turn);
+        p = 1;
+        
+        vTaskDelay(200/portTICK_PERIOD_MS);
+        bot_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
+        vTaskDelay(200/portTICK_PERIOD_MS);    
+        turnbot(turn);
+
+        for(int i=0;i<4;i++)
+        {
+            printf("sensor value[%d]: %f  ", i, sensor_value[i]);
+        }
+        printf("\n");
+    }
+       
+    else if (sensor_value[0] < 100 && sensor_value[1] < 100 && sensor_value[2] < 100 && sensor_value[3] < 100)
+    {
+        turn = 'B';
+        printf("turn detected: %c  ", turn);
+        r = 1;
+          
+        vTaskDelay(200/portTICK_PERIOD_MS);
+        bot_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
+        vTaskDelay(200/portTICK_PERIOD_MS);    
+        turnbot(turn);
+
+        for(int i=0;i<4;i++)
+        {
+            printf("sensor value[%d]: %f  ", i, sensor_value[i]);
+        }
+        printf("\n");
+    }   
+  }
 }
 
 void app_main()
